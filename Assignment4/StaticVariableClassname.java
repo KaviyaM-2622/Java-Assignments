e04a4aa9a16d1db74b6e4129704be7ed86a57190
Kaviya M <mkaviya2622@gmail.com>
2026-01-02 22:38:06 +0530

    Create StaticVariableClassname.java

---
+++ Assignment4/StaticVariableClassname.java
@@ -0,0 +1,18 @@
+package com.index;
+
+public class StaticVariable_Classname {
+	
+	static int x=30;
+	static final int y=10;
+
+	public static void main(String[] args) {
+		
+		System.out.println("StaticVariable" +StaticVariable_Classname.x);
+
+		System.out.println("StaticVariable" +StaticVariable_Classname.y);
+		
+		
+
+	}
+
+}
