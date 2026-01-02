1fbd494d1046122f0352985c7aed4f407cb147c3
Kaviya M <mkaviya2622@gmail.com>
2026-01-02 22:36:15 +0530

    Create SumofTwoLocalVariables.java

---
+++ Assignment4/SumofTwoLocalVariables.java
@@ -0,0 +1,14 @@
+package com.index;
+
+public class SumofTwo_LocalVariables {
+
+	public static void main(String[] args) {
+		int a=15;
+		int b=20;
+	
+		int sum=a+b;
+		
+		System.out.println("Sum of two="+sum);
+	}
+
+}
